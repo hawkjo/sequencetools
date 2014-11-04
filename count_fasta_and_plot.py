6982cf34015e5b9acb0d92b884b00abfeb96c6bf
John Hawkins <hawkjo@gmail.com>
2014-11-03 19:33:45 -0600

    Improvements to count_fasta_and_plot.py
    Turned into function
    Added plotting niceties

--- count_fasta_and_plot.py
+++ count_fasta_and_plot.py
@@ -1,69 +1,87 @@
 import sys
 import matplotlib.pyplot as plt
 
-if len(sys.argv) != 2:
-    sys.exit("Usage: count_fasta_and_plot.py <fasta file>")
-filename = sys.argv[1]
+def count_fasta_and_plot(   fasta_fname, 
+                            show=True, 
+                            stats_fname=None, 
+                            fig_fname=None,
+                            fig_title='Sequence Length Distribution' ):
 
-#-------------------------------------------------------------------------------
-# Read file, gather sequence lengths, count GC bases
-#-------------------------------------------------------------------------------
-seq_lens = []
-total_GC = 0
-l=0
-with open(filename) as f:
-    next(f)
-    for line in f:
-        if line[0] == '>':
-            seq_lens.append(l)
-            l=0
-        else:
-            line = line.strip()
-            l += len(line)
-            total_GC += sum(1 for c in line if c in 'GC')
-    seq_lens.append(l)
-total_bases = sum(seq_lens)
+    if stats_fname is not None:
+        out = open(stats_fname, 'w')
+    else:
+        out = sys.stdout
+    #-------------------------------------------------------------------------------
+    # Read file, gather sequence lengths, count GC bases
+    #-------------------------------------------------------------------------------
+    seq_lens = []
+    total_GC = 0
+    l=0
+    with open(fasta_fname) as f:
+        next(f)
+        for line in f:
+            if line[0] == '>':
+                seq_lens.append(l)
+                l=0
+            else:
+                line = line.strip()
+                l += len(line)
+                total_GC += sum(1 for c in line if c in 'GC')
+        seq_lens.append(l)
+    total_bases = sum(seq_lens)
 
-#-------------------------------------------------------------------------------
-# Calculate N-Statistics
-#-------------------------------------------------------------------------------
-seq_lens.sort(reverse=True)
-##################################################
-# Enter here the desired N-Statistic values 
-# !!!!  Must be in ascending order  !!!!
-Ns = [10,25,50,75,90] 
-##################################################
-running_base_count = 0
-running_percent = 0
-current_N_ind = 0
-N_seq_ind = []
-N_bp = []
-for i, l in enumerate(seq_lens):
-    running_base_count += l
-    prev_percent = running_percent
-    running_percent = float(running_base_count)/total_bases*100
-    if prev_percent < Ns[current_N_ind] <= running_percent:
-        N_seq_ind.append(i)
-        N_bp.append(l)
-        current_N_ind += 1
-        if current_N_ind >= len(Ns): break
+    #-------------------------------------------------------------------------------
+    # Calculate N-Statistics
+    #-------------------------------------------------------------------------------
+    seq_lens.sort(reverse=True)
+    ##################################################
+    # Enter here the desired N-Statistic values 
+    # !!!!  Must be in ascending order  !!!!
+    Ns = [10,25,50,75,90] 
+    ##################################################
+    running_base_count = 0
+    running_percent = 0
+    current_N_ind = 0
+    N_seq_ind = []
+    N_bp = []
+    for i, l in enumerate(seq_lens):
+        running_base_count += l
+        prev_percent = running_percent
+        running_percent = float(running_base_count)/total_bases*100
+        if prev_percent < Ns[current_N_ind] <= running_percent:
+            N_seq_ind.append(i)
+            N_bp.append(l)
+            current_N_ind += 1
+            if current_N_ind >= len(Ns): break
 
-#-------------------------------------------------------------------------------
-# Print and plot results
-#-------------------------------------------------------------------------------
-w = 35
-print 'Total length of sequence:'.ljust(w) + '%d bp' % total_bases
-print 'Total number of sequences:'.ljust(w) + str(len(seq_lens))
-for i in range(len(N_bp)):
-    print 'N%d:'.ljust(w) % Ns[i] + '%d bp, %d sequences' % (N_bp[i], N_seq_ind[i]+1)
-print 'Total GC count:'.ljust(w) + '%d bp' % total_GC
-print 'GC %:'.ljust(w) + '%.2f %%' % (float(total_GC)/total_bases*100)
+    #-------------------------------------------------------------------------------
+    # Print and plot results
+    #-------------------------------------------------------------------------------
+    w = 35
+    out.write( 'Total length of sequence:'.ljust(w) + '%d bp' % total_bases + '\n' )
+    out.write( 'Total number of sequences:'.ljust(w) + str(len(seq_lens)) + '\n' )
+    for i in range(len(N_bp)):
+        out.write( 'N%d:'.ljust(w) % Ns[i] + '%d bp, %d sequences' % (N_bp[i], N_seq_ind[i]+1) + '\n' )
+    out.write( 'Total GC count:'.ljust(w) + '%d bp' % total_GC + '\n' )
+    out.write( 'GC %:'.ljust(w) + '%.2f %%' % (float(total_GC)/total_bases*100) + '\n' )
+    out.close()
 
-plt.plot(seq_lens, linewidth=2)
-for i in range(len(N_seq_ind)):
-    plt.plot([N_seq_ind[i],N_seq_ind[i]], [0,N_bp[i]], linewidth=2, label='N%d'%(Ns[i]), zorder=-1)
-plt.xlabel('Sequence Length Rank')
-plt.ylabel('Sequence Length (bp)')
-plt.title('Sequence Length Distribution')
-plt.legend()
-plt.show()
+    if not show and fig_fname is None: return
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(seq_lens, linewidth=2)
+    for i in range(len(N_seq_ind)):
+        ax.plot([N_seq_ind[i],N_seq_ind[i]], [0,N_bp[i]], linewidth=2, label='N%d'%(Ns[i]), zorder=-1)
+    ax.set_xlabel('Sequence Length Rank')
+    ax.set_ylabel('Sequence Length (bp)')
+    ax.set_title(fig_title)
+    plt.legend()
+    if fig_fname is not None: fig.savefig( fig_fname )
+    if show: plt.show()
+    plt.close(fig)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        sys.exit("Usage: count_fasta_and_plot.py <fasta file>")
+    fasta_fname = sys.argv[1]
+    count_fasta_and_plot(fasta_fname)
