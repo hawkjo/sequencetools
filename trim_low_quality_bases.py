932a133a73a7cc38331f30c65a43f69fa2fccc23
John Hawkins <hawkjo@gmail.com>
2014-11-03 20:32:32 -0600

    trim_low_quality_bases.py -> pep8 compliant

--- trim_low_quality_bases.py
+++ trim_low_quality_bases.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
-import sys, fastq_tools
+import sys
+import fastq_tools
 
 if len(sys.argv) == 2:
-    fastq_tools.trim_low_quality_bases( sys.argv[1] )
+    fastq_tools.trim_low_quality_bases(sys.argv[1])
 elif len(sys.argv) == 3:
-    fastq_tools.trim_low_quality_bases( sys.argv[1], sys.argv[2] )
+    fastq_tools.trim_low_quality_bases(sys.argv[1], sys.argv[2])
 else:
     sys.exit('Usage: trim_low_quality_bases.py <fastq file> [<paired read fastq file>]')
