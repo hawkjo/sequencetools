fcc9d3cceacec7e1f88180228e948d3071d8c6a0
John Hawkins <hawkjo@gmail.com>
2014-11-03 20:33:10 -0600

    trim_single_read_adapters.py -> pep8 compliant

--- trim_single_read_adapters.py
+++ trim_single_read_adapters.py
@@ -1,6 +1,5 @@
-import sys, os
+import sys
 from adapters import trim_single_read_adapters
-from collections import Counter
 
 if len(sys.argv) != 2:
     sys.exit('Usage: trim_paired_read_adapters.py <fastq file>')
