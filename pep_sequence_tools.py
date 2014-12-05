9deea8fc4a2cdaa8872361a5b1ac540474967844
John Hawkins <hawkjo@gmail.com>
2014-12-05 15:03:06 -0600

    Added aa char <-> abr maps

--- pep_sequence_tools.py
+++ pep_sequence_tools.py
@@ -1,4 +1,17 @@
-aas = 'ARNDCQEGHILKMFPSTWYVBZX'
+#--------------------------------------------------------------------------------------------------
+# Initialize useful aa abr/char mappings and lists
+#--------------------------------------------------------------------------------------------------
+aa_char_given_abr = { 
+    'ALA': 'A', 'ARG': 'R', 'ASN': 'N', 'ASP': 'D', 'CYS': 'C', 'GLN': 'Q', 'GLU': 'E', 'GLY': 'G',
+    'HIS': 'H', 'ILE': 'I', 'LEU': 'L', 'LYS': 'K', 'MET': 'M', 'PHE': 'F', 'PRO': 'P', 'SER': 'S',
+    'THR': 'T', 'TRP': 'W', 'TYR': 'Y', 'VAL': 'V' 
+    }
+aa_abr_given_char = {char: abr for abr, char in aa_char_given_abr.items()}
+aas = aa_char_given_abr.values()
+aa_abrs = aa_char_given_abr.keys()
+aa_abrs.sort()
+aa_ind_given_abr = {abr: aa_abrs.index(abr) for abr in aa_abrs}
+aa_abr_given_ind = {aa_abrs.index(abr): abr for abr in aa_abrs}
 
 strong_groups = [set('STA'),
                  set('NEQK'),
