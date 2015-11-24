ad4bf6f0a384315794e2e74a634e110e6d041ebe
John Hawkins <hawkjo@gmail.com>
2015-11-23 20:01:12 -0600

    Added hamming_with_N

--- adapters_cython.pyx
+++ adapters_cython.pyx
@@ -24,6 +24,19 @@ cdef int hamming_distance(char *read,
 def simple_hamming_distance(first, second):
     return hamming_distance(first, second, len(first), len(second), 0)
 
+cdef int cython_hamming_with_N(char *s1, char *s2, int compare_length):
+    cdef int mismatches = 0
+    cdef int i
+
+    for i in range(compare_length):
+        if 'N' != s1[i] != s2[i] != 'N':
+            mismatches += 1
+
+    return mismatches
+
+def simple_hamming_with_N(first, second):
+    return cython_hamming_with_N(first, second, min(len(first), len(second)))
+
 def find_adapter_positions(read, adapter, int min_comparison_length, int max_distance):
     cdef int read_length = len(read)
     cdef int adapter_length = len(adapter)
