05433336dea853415e92ecacb8530ad63748272c
John Hawkins <hawkjo@gmail.com>
2015-11-23 20:03:37 -0600

    Added and modified functions
    Added:
        extract_cds_and_protein_fasta_from_rna
    
    Modified:
        iterate_ncbi_rna_cds_and_translation

--- gbtools.py
+++ gbtools.py
@@ -1,27 +1,42 @@
+import os
 from copy import deepcopy
 from itertools import izip
 from Bio import SeqIO
+from Bio import Seq
 from Bio.SeqFeature import FeatureLocation
 from misctools import gzip_friendly_open
 from general_sequence_tools import aa_or_X_given_codon
 
 def iterate_ncbi_rna_cds_and_tranlation(fpath):
     """From NCBI RNA annotation file, iterate over CDSs and tranlations."""
-    standard_mismatches = set(['X*'])  # NCBI translates * as X
+    standard_mismatches = set(['X*', 'U*'])  # NCBI often translates * as X
+
+    def get_singular_qualifier(feature, qual_name):
+        """Return qualifier known to have exactly one entry given feature and qualifier name."""
+        quals = feature.qualifiers[qual_name]
+        assert len(quals) == 1, str(quals)
+        return quals[0]
+
     for rec in SeqIO.parse(gzip_friendly_open(fpath), 'gb'):
         cds_feats = [feat for feat in rec.features if feat.type.upper() == 'CDS']
         if not cds_feats:
             continue
         assert len(cds_feats) == 1, rec.name
         cds_feat = cds_feats[0]
-        assert len(cds_feat.qualifiers['translation']) == 1
-        ncbi_translation = str(cds_feat.qualifiers['translation'][0])
-        codon_start = int(cds_feat.qualifiers['codon_start'][0])
+
+        gene_name = get_singular_qualifier(cds_feat, 'gene')
+        protein_id = get_singular_qualifier(cds_feat, 'protein_id')
+        ncbi_translation = get_singular_qualifier(cds_feat, 'translation')
+        codon_start = int(get_singular_qualifier(cds_feat, 'codon_start'))
         cds, cds_translation = cds_extract(rec.seq, cds_feat.location, codon_start)
-        mismatches = set(c1+c2 for c1, c2 in izip(ncbi_translation, cds_translation) if c1 != c2)
-        assert mismatches <= standard_mismatches
-        gene_name = cds_feat.qualifiers['gene']
-        yield rec.name, gene_name, cds, ncbi_translation
+        # Note that mismatching first aa is handled separately for alternative start codons
+        mismatches = set(c1+c2 for c1, c2 in
+                         izip(ncbi_translation[1:], cds_translation[1:]) if c1 != c2)
+        if ncbi_translation[0] != 'M' and ncbi_translation[0] != cds_translation[0]:
+            mismatches.add(ncbi_translation[0] + cds_translation[0])
+        assert mismatches <= standard_mismatches, \
+                '\n'.join([repr(mismatches), rec.name, ncbi_translation, cds_translation])
+        yield rec, gene_name, rec.name, protein_id, cds, ncbi_translation
 
 
 def cds_extract(seq, location, codon_start=1):
@@ -61,3 +76,20 @@ def cds_extract(seq, location, codon_start=1):
         cds = cds[:-3]
         cds_translation = cds_translation[:-1]
     return cds, cds_translation
+
+def extract_cds_and_protein_fasta_from_rna(fpath):
+    basename = os.path.splitext(fpath)[0]
+    cds_fpath = basename + '_extracted_cds.fa'
+    prot_fpath = basename + '_extracted_protein.fa'
+    with open(cds_fpath, 'w') as cds_out, open(prot_fpath, 'w') as prot_out:
+        for rec, gene_id, rna_id, prot_id, cds, prot in iterate_ncbi_rna_cds_and_tranlation(fpath):
+            cds_rec = SeqIO.SeqRecord(Seq.Seq(cds.upper()),
+                    rna_id,
+                    '',
+                    'gene_id=%s prot_id=%s' % (gene_id, prot_id))
+            prot_rec = SeqIO.SeqRecord(Seq.Seq(prot.upper()),
+                    prot_id,
+                    '',
+                    'gene_id=%s rna_id=%s' % (gene_id, rna_id))
+            SeqIO.write(cds_rec, cds_out, 'fasta')
+            SeqIO.write(prot_rec, prot_out, 'fasta')
