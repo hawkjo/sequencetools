2471bbd1048986526d80348b16059e0d773765fe
John Hawkins <hawkjo@gmail.com>
2015-11-23 20:00:52 -0600

    Updated build_adapters

--- adapters.py
+++ adapters.py
@@ -2,7 +2,7 @@ import sys
 import os
 from collections import Counter
 from adapters_cython import find_adapter_positions
-from fastq_tools import get_GSAF_barcode
+from fastqtools import get_GSAF_barcode
 from contaminants import get_fastqc_contaminant_list, output_contaminant_removal_statistics
 
 tru_seq_R1_rc = 'AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGTAGA'
@@ -18,9 +18,17 @@ P7_rc = 'ATCTCGTATGCCGTCTTCTGCTTG'
 A_tail = 'A' * 10
 
 
-def build_adapters(index_sequence='', max_length=None):
-    adapter_in_R1 = tru_seq_R2_rc + index_sequence + P7_rc + A_tail
-    adapter_in_R2 = tru_seq_R1_rc + P5_rc + A_tail
+def build_adapters(index_sequence='', max_length=None, primer_type='tru_seq'):
+    if primer_type == 'tru_seq':
+        R1_rc = tru_seq_R1_rc
+        R2_rc = tru_seq_R2_rc
+    elif primer_type == 'PE':
+        R1_rc = paired_end_R1_rc
+        R2_rc = paired_end_R2_rc
+    else:
+        raise ValueError('Invalid primer type: {0}'.format(primer_type))
+    adapter_in_R1 = R2_rc + index_sequence + P7_rc + A_tail
+    adapter_in_R2 = R1_rc + P5_rc + A_tail
     truncated_slice = slice(None, max_length)
     return adapter_in_R1[truncated_slice], adapter_in_R2[truncated_slice]
 
